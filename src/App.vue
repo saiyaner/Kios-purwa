0b1e9e888fff480662b948b7d60d469a4f3e5c2c
saiyaner <anggalesmana2006@gmail.com>
2026-05-14 23:00:02 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
 <template>
-  <div class="h-screen flex items-center justify-center bg-black">
-    <h1 class="text-5xl font-bold text-green-400">
-      Tailwind Berhasil 🚀
-    </h1>
-  </div>
+  
 </template>
