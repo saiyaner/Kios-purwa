dc5d0deaada65a039475b62d68929307a998bf75
Harvertz-9 <thismyproject25t@gmail.com>
2026-05-14 23:24:18 +0800

    test setup tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <button
-    class="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-xl text-white transition"
+    class="text-center px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-xl text-white transition"
   >
     Klik Saya
+  </button>
+  <button
+    class="text-center px-4 py-2 bg-red-500 hover:bg-red-600 rounded-xl text-white transition"
+  >
+    Klik Saya 2
   </button>
 </template>
